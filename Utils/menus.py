9c7c5f3d42c3e38fb82cce7519fa126d5ea5dc14
Motonory <matveycodes835@gmail.com>
2025-09-13 19:00:47 +0300

    Revert "mini-fix"
    This reverts commit af5c43efe00e2b2a3682ab3ede132b1037915227.

--- Utils/menus.py
+++ Utils/menus.py
@@ -4,10 +4,11 @@ from settings import *
 
 settings_menu = ft.Row([
     checkbox_theme
-], ft.MainAxisAlignment.CENTER)
-
-main_menu = ft.Column([
-    ft.Row([dropdown_type, name_type, value], ft.MainAxisAlignment.CENTER),
-    ft.Row([btn_g], ft.MainAxisAlignment.CENTER),
-    ft.Row([code_container], ft.MainAxisAlignment.CENTER)
+])
+main_menu =ft.Row([
+    dropdown_type,
+    name_type,
+    value,
+    code_container,
+    btn_g
 ])
